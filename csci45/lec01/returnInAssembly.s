c670fda4b4460bda846c59e2279cce34c8af5efc
Lawton Nichols <lawtonnichols@gmail.com>
2022-08-08 14:29:26 -0700

    .

---
+++ csci45/lec01/returnInAssembly.s
@@ -0,0 +1,8 @@
+.global _start  // _start is a global label
+_start:
+  @ this is also a comment
+  /* this too */
+  mov r0, #55  // move 55 into the register r0
+  mov r7, #1   // move 1 into the register r7
+  // 1 is the code for the "exit" system call
+  swi 0  // hey Linux kernel, please execute the system call that I just set up!
