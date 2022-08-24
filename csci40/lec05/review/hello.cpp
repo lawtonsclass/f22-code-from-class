6c5295c595a9ebef6c8f0739bef0ac5877ea9269
Lawton Nichols <lawtonnichols@gmail.com>
2022-08-23 17:10:01 -0700

    .

---
+++ csci40/lec05/review/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello, world!\n";
+  cout << "Hello, world!" << endl;
+
+  int i;
+  cout << i << endl; // undefined
+  i = 5;
+
+  return 0; // success
+}
