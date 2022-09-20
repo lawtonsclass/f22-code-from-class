f1915d1f8e5f9267e0d7fa85186534922f4980a9
Lawton Nichols <lawtonnichols@gmail.com>
2022-09-20 16:41:53 -0700

    .

---
+++ csci40/lec12/sum_of_squares/sumOfSquares.h
@@ -0,0 +1,11 @@
+#ifndef SUMOFSQUARES_H
+#define SUMOFSQUARES_H
+
+#include "sumOfSquares.h"
+
+// function declarations go in the .h file
+int sumSquaresFrom1To(int n);
+int square(int x);
+int add(int x, int y);
+
+#endif
