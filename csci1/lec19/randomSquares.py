15b1695cd59fca0ff5c49e3c24b0d35c96fe0fa0
Lawton Nichols <lawtonnichols@gmail.com>
2022-10-17 17:06:52 -0700

    .

---
+++ csci1/lec19/randomSquares.py
@@ -0,0 +1,35 @@
+import turtle
+import random
+
+turtle.shape("turtle")
+turtle.speed("fastest")
+
+i = 1
+while i <= 50:
+    # pick a random fill color
+    colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+    random_index = random.randint(0, len(colors) - 1)
+    turtle.fillcolor(colors[random_index])
+    
+    starting_x = random.randint(-200, 200)
+    starting_y = random.randint(-200, 200)
+    square_side_length = random.randint(50, 200)
+
+    # go to the random starting point
+    turtle.up()
+    turtle.goto(starting_x, starting_y)
+    turtle.down()
+
+    # draw a square there with the correct random size
+    turtle.begin_fill()
+    turtle.forward(square_side_length)
+    turtle.left(90)
+    turtle.forward(square_side_length)
+    turtle.left(90)
+    turtle.forward(square_side_length)
+    turtle.left(90)
+    turtle.forward(square_side_length)
+    turtle.end_fill()
+
+    i = i + 1
+
